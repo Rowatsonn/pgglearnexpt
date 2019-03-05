b92afbc99e8bcd541890aa90b82cfc45f26f50ef
thomasmorgan <thomas.j.h.morgan@gmail.com>
2019-03-05 08:22:10 -0700

    Fix indentation

--- robinpgg/experiment.py
+++ robinpgg/experiment.py
@@ -66,8 +66,8 @@ class pgglearn(Experiment):
     		self.recruiter.close_recruitment()
 
     def node_post_request(self, participant, node):
-	if node.network.full:
-		node.network.nodes(type=QuizSource)[0].transmit()
-	else:
- 		pass # This is actually redundant, but is here for completeness     
-    	
+        if node.network.full:
+            node.network.nodes(type=QuizSource)[0].transmit()
+        else:
+            pass # This is actually redundant, but is here for completeness     
+	
