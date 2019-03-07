5d1fce5a60df118305b5c2a7ef3b90d47c515682
Ruben222 <47988553+Ruben222@users.noreply.github.com>
2019-03-07 15:10:35 -0700

    Add files via upload

---
+++ robinpgg/robinpgg/models.py
@@ -0,0 +1,117 @@
+from dallinger.nodes import Source
+from dallinger.models import Node 
+
+
+class QuizSource(Source):
+    """A Source that reads a question and transmits it. The question is transmitted along with the 
+    correct and incorrect answers."""
+
+    __mapper_args__ = {
+        "polymorphic_identity": "quiz_source"
+    }
+
+    def _contents(self):
+        """Define the contents of new Infos .... (New transmissions??)
+
+        transmit() -> _what() -> create_information() -> _contents().
+        """
+        number_transmissions = len(self.infos())
+        import json
+        questions = [
+            json.dumps({
+                'question': 'When did the First World War end?',
+                'number' : 1,
+                'Wwer1': '1914',
+		'Wwer2': '1939',
+                'Rwer': '1918',
+                }),
+            json.dumps({
+                'question': 'What is the hottest planet in the solar system?',
+                'number': 2,
+                'Wwer1': 'Mercury',
+		'Wwer2': 'Jupiter',
+                'Rwer': 'Venus',
+                }),
+            json.dumps({
+                'question': 'Which actor played Saul Goodman in the show Breaking Bad?',
+                'number': 3,
+                'Wwer1': 'Aaron Paul',
+		'Wwer2': 'Jonathan Banks',
+                'Rwer': 'Bob Odenkirk',
+                }),
+            json.dumps({
+                'question': 'Vincent van Gogh died in which year?',
+                'number': 4,
+                'Wwer1': '1880',
+		'Wwer2': '1900',
+                'Rwer': '1890',
+                }),
+            json.dumps({
+                'question': 'Which of these countries has the SMALLEST population?',
+                'number': 5,
+                'Wwer1': 'India',
+		'Wwer2': 'United States of America',
+                'Rwer': 'Russia',
+                }),
+            json.dumps({
+		'question': 'In which year was the Magna Carta signed?',
+                'number': 6,
+                'Wwer1': '1066',
+		'Wwer2': '1775',
+                'Rwer': '1215',
+                }),
+            json.dumps({
+                'question': 'Which of these US presidents was president FIRST?',
+                'number': 7,
+                'Wwer1': 'Richard Nixon',
+		'Wwer2': 'John F. Kennedy',
+                'Rwer': 'Franklin D. Roosevelt',
+                }),
+            json.dumps({
+                'question': 'In Which continent is Mt. Everest located?',
+                'number': 8,
+                'Wwer1': 'Austrailia (Oceania)',
+		'Wwer2': 'Europe',
+                'Rwer': 'Asia',
+                }),
+            json.dumps({
+		'question': 'Which of these chemical elements is a Halogen?',
+                'number' : 9,
+                'Wwer1': 'Xenon',
+		'Wwer2': 'Phosphorus',
+                'Rwer': 'Iodine',
+                }),
+            json.dumps({
+                'question': 'Which of these Shakespeare plays was written FIRST?',
+                'number': 10,
+                'Wwer1': 'Macbeth',
+		'Wwer2': 'Romeo and Juiliet',
+                'Rwer': 'The Comedy of Errors',
+                }),
+             json.dumps({
+                'question': 'This is a dummy question, if you see this. It gone broke.',
+                'number': 11,
+                'Wwer1': 'Woops',
+		'Wwer2': 'Oh well',
+                'Rwer': 'dallinger debug',
+                }),
+        ]
+
+        return questions[number_transmissions]
+
+class ProbeNode(Node):
+
+    __mapper_args__ = {
+        "polymorphic_identity": "probe_node"
+    }
+
+    @property
+    def score_in_quiz(self):
+        return json.loads(self.property1)["score_in_quiz"]
+
+    @score_in_quiz.setter
+    def score_in_quiz(self, val):
+        p1 = json.loads(self.property1)
+        p1["score_in_quiz"] = val
+        self.property1 = json.dumps(p1)
+
---
+++ robinpgg/robinpgg/static/scripts/experiment.js
@@ -0,0 +1,218 @@
+var my_node_id;
+
+// Consent to the experiment.
+$(document).ready(function() {
+
+  // do not allow user to close or reload
+  dallinger.preventExit = true;
+
+  // Print the consent form.
+  $("#print-consent").click(function() {
+    window.print();
+  });
+
+  // Consent to the experiment.
+  $("#consent").click(function() {
+    store.set("recruiter", dallinger.getUrlParameter("recruiter"));
+    store.set("hit_id", dallinger.getUrlParameter("hit_id"));
+    store.set("worker_id", dallinger.getUrlParameter("worker_id"));
+    store.set("assignment_id", dallinger.getUrlParameter("assignment_id"));
+    store.set("mode", dallinger.getUrlParameter("mode"));
+
+    dallinger.allowExit();
+    dallinger.goToPage('instructions');
+  });
+
+  // Consent to the experiment.
+  $("#no-consent").click(function() {
+    dallinger.allowExit();
+    window.close();
+  });
+
+  // Consent to the experiment.
+  $("#go-to-experiment").click(function() {
+    dallinger.allowExit();
+    dallinger.goToPage('exp'); //Make sure we change that 
+  });
+
+  $("#submit-response").click(function() {
+    $("#submit-response").addClass('disabled');
+    $("#submit-response").html('Sending...');
+    dallinger.createInfo(my_node_id, {contents: "Submitted", info_type: "Info"})
+    .done(function (resp) {
+      dallinger.allowExit();
+      dallinger.goToPage('questionnaire');
+    })
+    .fail(function (rejection) {
+      dallinger.allowExit();
+      dallinger.error(rejection);
+    });
+  });
+});
+
+// Create the agent.
+var create_agent = function() {
+  // Setup participant and get node id
+  $("#submit-response").addClass('disabled');
+  dallinger.createAgent()
+  .done(function (resp) {
+    my_node_id = resp.node.id;
+    $("#submit-response").removeClass('disabled');
+    get_transmissions(my_node_id); //It starts constantly checking it's transmissions
+  })
+  .fail(function (rejection) {
+    dallinger.allowExit();
+    dallinger.error(rejection);
+  });
+};
+
+// Get a nodes transmissions. This runs constanly once create_agent has run, which it will once you load up the html page 
+var get_transmissions = function() {
+    dallinger.getTransmissions(my_node_id, {
+        status: "pending"
+    })
+    .done(function (resp) {
+        transmissions = resp.transmissions;
+        if (transmissions.length > 0) {
+            if (transmissions.length > 1) {
+                console.log("More than one transmission - unexpected!");
+            } else {
+                get_info(transmissions[0].info_id); // This calls the function below. 
+            }
+        } else {
+            setTimeout(function(){
+                get_transmissions();
+            }, 1000); // Unless the node finds some info, it just calls get_transmissions every second. 
+        }
+    })
+    .fail(function (rejection) {
+        console.log(rejection);
+        $('body').html(rejection.html);
+    });
+}
+
+// Get the nodes info. resp is a generic term that servers use for a response from a browser. 
+var get_info = function(info_id) {
+    dallinger.getInfo(my_node_id, info_id)
+    .done(function(resp) {
+        process_info(resp.info); // This is yet another function call. Also resp, contains an info. 
+    })
+    .fail(function (rejection) {
+        console.log(rejection);
+        $('body').html(rejection.html);
+    });
+}
+
+// Process the info that the node receievs.
+var process_info = function(info) {
+    parse_question(info);
+    if (number == 11) {
+        dallinger.allowExit();
+        dallinger.goToPage('scorescreen');
+    } else {
+        display_question(); // This is another function call 
+        }
+}; 
+
+// This sets the question up ready to be displayed.
+var parse_question = function(question) {
+    question_json = JSON.parse(question.contents);
+    question_text = question_json.question;
+    Wwer1 = question_json.Wwer1;
+    Wwer2 = question_json.Wwer2
+    Rwer = question_json.Rwer;
+    number = question_json.number;
+}
+
+
+// Display the question 
+var display_question = function() {
+    $("#question").html(question_text);
+    $("#question_number").html(number);
+
+    var num = Math.random(); 
+  
+     if (num <0.33) {
+       $("#submit-a").html(Rwer);
+       if (Math.random() <0.5 ){
+           $("#submit-b").html(Wwer1);
+           $("#submit-c").html(Wwer2);
+           } else {
+             $("#submit-b").html(Wwer2);
+             $("#submit-c").html(Wwer1); 
+           }
+     } else if (num >0.33 && num <0.66 ) {
+       $("#submit-b").html(Rwer);
+       if (Math.random() <0.5 ){
+           $("#submit-a").html(Wwer1); 
+           $("#submit-c").html(Wwer2);
+           } else {
+           $("#submit-a").html(Wwer2); 
+           $("#submit-c").html(Wwer1);
+           }
+     } else {
+       $("#submit-c").html(Rwer); 
+       if (Math.random() <0.5){
+         $("#submit-a").html(Wwer1);
+         $("#submit-b").html(Wwer2);
+       } else {
+         $("#submit-a").html(Wwer2);
+         $("#submit-b").html(Wwer1);
+       }
+ }; //End of the if else statement 
+    enable_answer_buttons(); //Calls another two functions below
+    countdown = 15; // This can set the time they have to answer
+    start_answer_timeout();
+}; // End of the function
+
+// Start a timer to countdown to when the participant needs to have answered by
+var start_answer_timeout = function() {
+    $("#countdown").show();
+    answer_timeout = setTimeout(function() {
+        countdown = countdown - 1;
+        $("#countdown").html(countdown);
+        if (countdown <= 0) {
+            disable_answer_buttons(); // Calls another function
+            $("#countdown").hide();
+            $("#countdown").html("");
+            submit_response(Wwer1);
+            
+        } else {
+            start_answer_timeout();
+        }
+    }, 1000);
+}
+
+// Displays and engages the answer buttons
+var enable_answer_buttons = function() {
+    $("#submit-a").removeClass('disabled');
+    $("#submit-b").removeClass('disabled');
+    $("#submit-c").removeClass('disabled');
+    $("#submit-a").show();
+    $("#submit-b").show();
+    $("#submit-c").show(); 
+}
+
+// Hides and disengages the answer buttons
+var disable_answer_buttons = function() {
+    $("#submit-a").addClass('disabled');
+    $("#submit-b").addClass('disabled');
+    $("#submit-c").addClass('disabled');
+    $("#submit-a").hide();
+    $("#submit-b").hide();
+    $("#submit-c").hide();
+    $("#question").html("Waiting for the next question..."); 
+}
+
+//submit_response still needs to be defined properly
+
+var submit_response = function(value) {
+    console.log("Hello " + value)
+    data = {
+        "contents": value
+    }
+    dallinger.createInfo(my_node_id, data)
+    .done(get_transmissions());
+    countdown = 10000; // This is to stop some bug where it would double submit answers. 
+}
+
