8e892702fce39502b6abb7dca7c3f35e9b38dc1e
Ruben222 <47988553+Ruben222@users.noreply.github.com>
2020-07-30 10:01:17 +0100

    Javascript exploitation measures

--- static/scripts/experiment.js
+++ static/scripts/experiment.js
@@ -19,8 +19,8 @@ $(document).ready(function() {
 // Starts a timer to display a new message if participants end up waiting for too long. Implies somebody has gone AFK. 
 // This calls in the PGG and the quiz once the participant joins / makes a choice.
 var start_AFK_timeout = function(){
-  console.log("Start AFK Timeout was called")
-  console.log(afk_countdown)
+  //console.log("Start AFK Timeout was called")
+ // console.log(afk_countdown)
   afk_countdown = afk_countdown- 1
   if (afk_countdown <= 0){
     $("#long_time").show(); // This is a HTML element on both trivia and pgg which just states that participants should keep the window open.
@@ -72,9 +72,9 @@ var toggleSound = function() {
 // Create the agent.
 var create_agent = function() {
   // Setup participant and get node id
-  console.log("Create agent called")
+  //console.log("Create agent called")
   afk_countdown = 30 // Sets the AFK timeout.
-  console.log("afk countdown is " + afk_countdown) 
+  //console.log("afk countdown is " + afk_countdown) 
   $("#submit-response").addClass('disabled');
   dallinger.createAgent()
   .done(function (resp) {
@@ -97,7 +97,7 @@ var create_agent = function() {
 
 // Get a nodes transmissions. This runs constanly once create_agent has run, which it will once you load up the html page 
 var get_transmissions = function() {
-    console.log("get transmissions was called");
+    //console.log("get transmissions was called");
     if (trivia_started == 1) { // Countdown does not run if the trivia hasn't started. In other words, people are still joining.
     start_AFK_timeout(); // Starts the timeout in the background for the AFK message to come up
     }
@@ -106,10 +106,10 @@ var get_transmissions = function() {
     })
     .done(function (resp) {
         transmissions = resp.transmissions;
-        console.log(transmissions)
+        //console.log(transmissions)
         if (transmissions.length > 0) {
             if (transmissions.length > 1) {
-                console.log("More than one transmission - unexpected!");
+               // console.log("More than one transmission - unexpected!");
             } else {
                 get_info(transmissions[0].info_id); // This calls the function below. 
             }
@@ -123,7 +123,7 @@ var get_transmissions = function() {
         }
     })
     .fail(function (rejection) {
-        console.log(rejection);
+        //console.log(rejection);
         $('body').html(rejection.html);
     });
 }
@@ -151,13 +151,13 @@ var get_info = function(info_id) {
     $("#totalpps").hide();
     $("#slash").hide()
     afk_countdown = 30 // Resets the AFK timer. Don't want the AFK message popping up unannounced.
-    console.log("successfully set the AFK countdown to" + afk_countdown) 
+   // console.log("successfully set the AFK countdown to" + afk_countdown) 
     dallinger.getInfo(my_node_id, info_id)
     .done(function(resp) {
         process_info(resp.info); // This is yet another function call. Also resp, contains an info. 
     })
     .fail(function (rejection) {
-        console.log(rejection);
+       // console.log(rejection);
         $('body').html(rejection.html);
     });
 }
@@ -167,7 +167,7 @@ var get_info = function(info_id) {
 var process_info = function(info) {
     parse_question(info);
     if (number == 11) {
-        console.log("number identified as 11")
+       // console.log("number identified as 11")
         submit_response(Wwer1, human=false); // Creates an info just to signal Q is done. To not break the network again later.
     } else {
         display_question(); // This is another function call 
@@ -301,7 +301,7 @@ var submit_response = function(answer, human=true) {
         dallinger.goToPage('score');
         } else {
         test = resp
-        console.log(resp)
+        //console.log(resp)
 	get_transmissions()
         }
   });  
@@ -311,7 +311,7 @@ var submit_response = function(answer, human=true) {
 
 // Finds and saves the ID of the Pog as a cookie. Calls on score.html
 var save_pog = function(){
-  console.log("Called save pog")
+ // console.log("Called save pog")
   my_node_id = dallinger.storage.get("my_node");
   dallinger.get(
   "/node/" + my_node_id + "/neighbors",
@@ -321,8 +321,8 @@ var save_pog = function(){
   }
 ).done(function(resp){
     pog = resp.nodes
-    console.log("This is the pog")
-    console.log(pog)
+   // console.log("This is the pog")
+   // console.log(pog)
     if (pog.length > 0){
       pog.forEach(function(node){
         pog_id = node.id
@@ -355,8 +355,8 @@ var hide_blank = function() {
 
 // Checks for all neighbors of the pog with a to connection
 var check_neighbors = function(pog_id){
-    console.log("check_neighbors was called")
-    console.log("pog_id is" + pog_id)
+   // console.log("check_neighbors was called")
+   // console.log("pog_id is" + pog_id)
     dallinger.get(
         "/node/" + pog_id + "/neighbors",
         {
@@ -365,8 +365,8 @@ var check_neighbors = function(pog_id){
     ).done(function (resp) {
         MYID = check_ID(); // Calls check ID for use in parse_neighbors.
         neighbors = resp.nodes;
-        console.log("These are the neighbors")
-        console.log(neighbors)
+       // console.log("These are the neighbors")
+       // console.log(neighbors)
         if (neighbors.length == 0) {
           setTimeout(function(){
             check_neighbors()
@@ -379,7 +379,7 @@ var check_neighbors = function(pog_id){
 
 // After getting said neighbors. This interprets them.
 var parse_neighbors = function(neighbors) {
-    console.log("Parse neighbors was called")
+   // console.log("Parse neighbors was called")
     neighbors.forEach(function(node) {
     score = JSON.parse(node.property1).score_in_quiz;
     prestige = JSON.parse(node.property2).prestige;
@@ -425,7 +425,7 @@ var display_score_you = function(score , id){
 // Beginning of code for the PGG instructions
 
 var begin_instructions = function(){
-  console.log("Begin instructions was called")
+//  console.log("Begin instructions was called")
   my_node_id = dallinger.storage.get("my_node");
   ping_server(my_node_id);
   dallinger.get(
@@ -436,18 +436,18 @@ var begin_instructions = function(){
   }
 ).done(function(resp) {
     pog = resp.nodes
-    console.log(pog)
+   // console.log(pog)
     pog.forEach(function(node){
       id = node.id
       snowdrift = JSON.parse(node.property3).snowdrift;
-      console.log(snowdrift)
+    //  console.log(snowdrift)
       dallinger.storage.set("snowdrift" , snowdrift); // Sets the condition as a cookie
       if(snowdrift == 1){
-         console.log("It's a SD")
+       //  console.log("It's a SD")
          $("#SD").show();
          $("#next").show();
       } else {
-         console.log("It's a PD")
+       //  console.log("It's a PD")
          $("#PD").show();
          $("#next").show();
       }
@@ -457,7 +457,7 @@ var begin_instructions = function(){
 
 // Get's the infos for the node as a way to ping the server that they are still there
 var ping_server = function(){
-  console.log("Server Pinged");
+ // console.log("Server Pinged");
   my_node_id = dallinger.storage.get("my_node");
   dallinger.getInfos(my_node_id)
 }
@@ -479,7 +479,7 @@ var page_check = function(){
 // This calls on the final page (pgg_condition_check) to explain what information they will receieve OR to provide them with the opportunity to choose
 // their social learning (NOT YET PROGRAMMED OR HTML'd)
 var final_page = function(){
-  console.log("Final Page was called");
+ // console.log("Final Page was called");
   ping_server();
   condition = dallinger.storage.get("my_condition");
   if (condition == "BB"){
@@ -521,7 +521,8 @@ var show_experiment = function() {
   force_choice = Math.floor(Math.random() * 10) + 0;
   round += 1
   $("#instructions").show();
-  countdown = 16; // Set the desired countdown number here  
+  countdown = 16; // Set the desired countdown number here
+  submission_ready = 1 // Allow a submission  
   start_experiment_timeout(); 
 }
 
@@ -537,12 +538,12 @@ for (var i = 0; i < 11; i++){ // For every button on the screen
 }
 
 var start_experiment_timeout = function () {
-  console.log("start experiment timeout was called")
+//  console.log("start experiment timeout was called")
   experiment_timeout = setTimeout(function(){
     $("#countdown").show()
     countdown = countdown - 1;
     $("#countdown").html(countdown);
-    console.log(countdown)
+  //  console.log(countdown)
     if (countdown <=0) {
       hide_experiment();
       submit_choice(force_choice, human=false); // If a participant doesn't decide, a random number is submitted
@@ -552,23 +553,25 @@ var start_experiment_timeout = function () {
   }, 1000); 
 }
 
-//Submits the participants choice. Human defaults to true, unless the choice is random.                               
+//Submits the participants choice. Human defaults to true, unless the choice is random. 
 var submit_choice = function(value, human=true) {
+  if(submission_ready == 1) { // function only runs if the participant hasn't already submit a choice
     clearTimeout(experiment_timeout);
     hide_experiment();
-    dallinger.createInfo(my_node_id, {
+      dallinger.createInfo(my_node_id, {
       contents: value,
       property1: JSON.stringify({
         "human": human
       })
     }).done(get_pog()); // It's like above, only this time it starts checking its neighbors
+  }
 }
 
 // Get's hold of the pogbot and loops if all nodes haven't chosen yet.
 var get_pog = function (){ 
   pog_timeout = setTimeout(function() {
   start_AFK_timeout();
-  console.log("Get pog was called")
+//  console.log("Get pog was called")
   dallinger.get(
   "/node/" + my_node_id + "/neighbors",
   {
@@ -577,7 +580,7 @@ var get_pog = function (){
   }
 ).done(function (resp){
     pog = resp.nodes
-    console.log(pog)
+  // console.log(pog)
     pog.forEach(function(node){
       poground = JSON.parse(node.property2).round;
       pot = JSON.parse(node.property1).pot
@@ -593,7 +596,7 @@ var get_pog = function (){
 
 // Retrieves the nodes leftovers and the pot and works out how much they earned that round
 var get_results = function(pot) {
-  console.log("get results was called")
+//  console.log("get results was called")
   clearTimeout(pog_timeout);
   afk_countdown = 30 // Resets the AFK timer
   $("#long_time").hide();
@@ -618,7 +621,7 @@ var get_results = function(pot) {
 
 // Find the leftovers the participant had
 var check_nodes = function(neighbors) {
-  console.log("check nodes was called")
+//  console.log("check nodes was called")
   neighbors.forEach(function(node) { // The block of code below is extracting the relevent social information from the node. It takes the last element in the list.
     node_id = node.id;
     leftovers = JSON.parse(node.property4).leftovers;
@@ -638,7 +641,7 @@ var check_nodes = function(neighbors) {
 
 // Show the participant their results
 var display_results = function(round_earnings) {
-  console.log("display_results was called"); 
+//  console.log("display_results was called"); 
   result_countdown = 10; // How long can participants view this?
   $("#waiting").hide();
   $("#earnings").show();
@@ -646,7 +649,7 @@ var display_results = function(round_earnings) {
   $("#points").html(round_earnings);
   $("#added").show();
   if (my_info == "prestige"){
-    console.log("My prestige is " + last_prestige)
+ //   console.log("My prestige is " + last_prestige)
     $("#donate").show()
     $("#prestige").show()
     $("#donate").html(last_prestige)
@@ -670,7 +673,7 @@ start_timer_countdown();
 
 // In the full info condition, this function handles getting the last donation and ID for each participant and putting it on the table. It also identifies the prestigious and the winning node.
 var full_info = function(neighbors){
-  console.log("full info was called")
+//  console.log("full info was called")
   winning_score = 0
   $("#table").show(); // Displays the sentence above the table
   $("#full").show(); // Displays the table without total score
@@ -683,7 +686,7 @@ var full_info = function(neighbors){
   var neighborsLength = neighbors.length;
   for (var i = 0; i < neighborsLength; i++){ //For every neighbor
     node = neighbors[i]
-    console.log(node);
+ //   console.log(node);
     ID = node.participant_id;
     donation = JSON.parse(node.property4).donation;
     prestige = JSON.parse(node.property2).prestige;
@@ -705,7 +708,7 @@ var full_info = function(neighbors){
 
 // In the extra_info condition. Participants see the same information as in the full_info, only they also get a column specifying each participants current score. 
 var extra_info = function(neighbors){
-  console.log("extra info was called")
+//  console.log("extra info was called")
   winning_score = 0
   $("#table").show(); // Displays the sentence above the table
   $("#extra").show(); // Displays the table with total score
@@ -718,7 +721,7 @@ var extra_info = function(neighbors){
   var neighborsLength = neighbors.length;
   for (var i = 0; i < neighborsLength; i++){ //For every neighbor
     node = neighbors[i]
-    console.log(node);
+ //   console.log(node);
     ID = node.participant_id;
     donation = JSON.parse(node.property4).donation;
     prestige = JSON.parse(node.property2).prestige;
@@ -742,13 +745,13 @@ var extra_info = function(neighbors){
 
 // In the reg_pgg condition, this function handles getting the last donation and ID for each participant and putting it on the table.
 var reg_pgg = function(neighbors){
-  console.log("reg pgg was called")
+  //console.log("reg pgg was called")
   $("#full").show(); // Displays the table
   $("#table").show(); // Displays the sentence above the table
   var neighborsLength = neighbors.length;
   for (var i = 0; i < neighborsLength; i++){ //For every neighbor
     node = neighbors[i]
-    console.log(node);
+    //console.log(node);
     ID = node.participant_id;
     donation = JSON.parse(node.property4).donation;
     row_name = "#row" + (i+1);
@@ -763,10 +766,10 @@ var reg_pgg = function(neighbors){
 
 // Starts a timer for the scorescreen to keep everyone synced up
 var start_timer_countdown = function() {
-  console.log("start timer countdown was called")
+  //console.log("start timer countdown was called")
   results_timeout = setTimeout(function(){
     result_countdown = result_countdown - 1;
-    console.log(result_countdown);
+    //console.log(result_countdown);
     if (result_countdown <=0) {
       hide_results(); 
     } else {
@@ -778,7 +781,7 @@ var start_timer_countdown = function() {
 // Hides the results and recalls show expeirment if the rounds aren't finished.
 var hide_results = function() {
   clearTimeout(results_timeout);
-  console.log("hide results was called")
+  //console.log("hide results was called")
   $("#earnings").hide();
   $("#points").hide(); 
   $("#added").hide();
@@ -800,7 +803,7 @@ var hide_results = function() {
 
 // Hides the table rows. This is only needed in case somebody leaves. Otherwise, you are stuck with a redundant and confusing row in the table. 
 var hide_table = function(){
-  console.log("Hide Table was called")
+  //console.log("Hide Table was called")
   $("#full").hide();
   $("#extra").hide();
   $("#table").hide()
@@ -814,7 +817,7 @@ var hide_table = function(){
 }
 // Displays the HTML script telling the participants they scored 0 this round
 var fail_round = function(){
-  console.log("fail round was called")
+  //console.log("fail round was called")
   snow_countdown = 10; 
   $("#waiting").hide();
   $("#SD").show();
@@ -841,10 +844,10 @@ var fail_round = function(){
 
 // Starts a countdown to determine how long this is on screen for
 var start_snow_countdown = function(){
-  console.log("start snow countdown was called")
+  //console.log("start snow countdown was called")
   snow_timeout = setTimeout(function(){
     snow_countdown = snow_countdown - 1;
-    console.log(snow_countdown);
+   // console.log(snow_countdown);
     if(snow_countdown <=0){
       hide_snow();
     } else {
@@ -856,7 +859,7 @@ var start_snow_countdown = function(){
 // Hides the snowdrift information and recalls the experiment if the rounds aren't finished.
 var hide_snow = function(){
   clearTimeout(snow_timeout);
-  console.log("hide snow was called")
+  //console.log("hide snow was called")
   $("#SD").hide();
   $("#SD1").hide();
   $("#prestige").hide();
